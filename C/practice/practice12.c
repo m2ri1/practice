12f0f6fb9c5a4f1e310782197fab40e28aed401e
박미리 <parkmiri0722@gmail.com>
2023-04-18 10:31:21 +0900

    4/18

---
+++ C/practice/practice12.c
@@ -0,0 +1,60 @@
+/*#include <stdio.h>
+int main()
+{
+    int rabbit[11], a;
+    int b=1;
+    int c=2;
+    for (a=0; a<11; a++)
+    {
+        b=a+1;
+        c=a+b;
+        c=rabbit[a];
+    }
+    printf("%d", rabbit[10]);
+    return 0;
+
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    int a=1, b=2, c=0, i;
+    for(i=0; i<8; i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+
+    }
+    printf("%d", c);
+}*/
+
+/*#include <stdio.h>
+
+int fibo(int num);
+int main()
+{
+    int num;
+    printf("\n개월 수 입력: ");
+    scanf("%d", &num);
+    printf("총 토끼 쌍: %d", fibo(num+1));
+    return 0;
+}
+int fibo(int num)
+{
+    if(num == 0) return 0;
+    else if(num == 1) return 1;
+    else return fibo(num - 1) + fibo(num - 2);
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+    int a;
+    srand((unsigned)time(NULL));
+
+    printf("%d", 1+rand()%6);
+    return 0;
+}
