51675cddb144afb494c8f64d460fcd17c8cc60e1
parkmiri <parkmiri0722@gmail.com>
2023-06-09 23:53:09 +0900

    update

---
+++ C/project/project.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int input = 0;
+    printf("현재 장소의 온,습도를 측정하려면 1을 입력하세요");
+    scanf("%d", &input);
+    if (input == 1)
+    {
+     
+    }
+}
+
+int temperature()
+{
+
+}
+
+int humidity()
+{
+
+}
